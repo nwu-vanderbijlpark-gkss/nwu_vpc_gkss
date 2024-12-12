214b709c3a6a46d6784570d35e2ed0337598b89b
lethabo <shysnylethabo11@gmail.com>
2024-12-12 18:56:28 +0200

    community forum, wip

--- src/routes/community/[topic]/+page.svelte
+++ src/routes/community/[topic]/+page.svelte
@@ -1,9 +1,8 @@
 <script>
 	import { ArrowLeft, ChevronLeft, Dot, Forward, MessageCircleMore, ThumbsDown, ThumbsUp } from "lucide-svelte";
 
-    let {data} = $props();
 </script>
-<title>{data.topic} | NWU Vaal GKSS</title>
+<title>Topic | NWU Vaal GKSS</title>
 <div class="space-y-2">
     <div class="hover:shadow p-2">
         <div class="text-sm flex justify-between items-center">
@@ -16,7 +15,7 @@
             <button class="btn btn-ghost">Report</button>
         </div>
     <a href="/community/topic here">
-        <h3 class="text-2xl font-bold">{data.topic}</h3>
+        <h3 class="text-2xl font-bold">Topic</h3>
         <p class="text-sm text-gray-500 mt-2">
             Dive into a heated discussion on how emerging technologies are shaping our world. 
             This is your chance to voice opinions, challenge ideas, and 
