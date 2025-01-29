4b7b061973e713de0efc9124c1ca639f5cbbe382
Lethabo <shysnylethabo11@gmail.com>
2025-01-29 11:46:08 +0200

    update: pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,10 @@
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
+	import PwaInstallPrompt from '../components/PWAInstallPrompt.svelte';
 	let { children, data } = $props();
 </script>
+<PwaInstallPrompt/>
 <Header isLoggedIn={data.isLoggedIn} isExecutive={data.isExecutive}/>
 <div class="mt-[65px] min-h-screen bg-gray-200">
 {@render children()}
--- src/routes/executive/+layout.svelte
+++ src/routes/executive/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import { ChartNoAxesColumn, HeartHandshake, Home, Link, LucideUsers, MessageCircleMoreIcon, User, UserCog, Users } from 'lucide-svelte';
 	import { slide } from 'svelte/transition';
+	import PwaInstallPrompt from '../../components/PWAInstallPrompt.svelte';
 
     let {children} = $props();
 </script>
 <title>Executive | NWU Vaal GKSS </title>
+
 <div class="flex min-h-screen w-full bg-gradient-to-b from-white to-gray-200 lg:p-1 justify-between">
     <ul class="menu bg-base-200 rounded-box w-56 min-h-[90svh] hidden lg:flex">
         <li class="menu-title flex bg-gray-800 text-white rounded-box my-2">
