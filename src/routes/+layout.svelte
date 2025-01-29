c21c2a7aa18cd39fc1498b7bd680bc71b7ce54aa
Lethabo <shysnylethabo11@gmail.com>
2025-01-29 11:19:11 +0200

    update: header links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,9 @@
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
 	import Header from '../components/Header.svelte';
-	let { children } = $props();
+	let { children, data } = $props();
 </script>
-<Header/>
+<Header isLoggedIn={data.isLoggedIn} isExecutive={data.isExecutive}/>
 <div class="mt-[65px] min-h-screen bg-gray-200">
 {@render children()}
 </div>
