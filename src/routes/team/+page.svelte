3e2960b906d000bdce615c82620b6889cd0ed264
lethabo <shysnylethabo11@gmail.com>
2025-02-12 17:43:37 +0200

    fix: team page

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -2,24 +2,34 @@
 	let { data, form } = $props();
 </script>
 
-<title>Team | NWU Vaal GKSS </title>
+<title>Team | NWU Vaal GKSS</title>
 <meta property="og:site_name" content="Team | NWU Vaal GKSS" />
 <meta name="twitter:title" content="Team | NWU Vaal GKSS" />
 <meta name="twitter:image:alt" content="Team | NWU Vaal GKSS" />
 <meta property="title" content="Team" />
-<div class="min-h-screen space-y-3 p-5">
-	<h1 class="text-2xl font-bold text-white">Meet the team</h1>
-	<div class="flex flex-col items-center justify-center gap-5 p-2 lg:grid lg:grid-cols-4">
+
+<div class="min-h-screen space-y-8 bg-gray-100 p-6">
+	<h1 class="text-center text-4xl font-extrabold text-black">Meet the Team</h1>
+	<div class="grid grid-cols-1 gap-6 p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 		{#each data.team as team}
-			<div class="card w-full rounded bg-white p-3 shadow-xl lg:w-[300px]">
-				<figure>
-					<img src={team.image} alt="member" class="lg:h-[400px]" />
+			<div
+				class="card transform overflow-hidden rounded-lg bg-white p-4 shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl"
+			>
+				<figure class="relative">
+					<img
+						src={team.image}
+						alt="member"
+						class="h-[300px] w-full transform rounded-md object-cover transition duration-300 ease-in-out hover:scale-105"
+					/>
 				</figure>
-				<div class="card-body px-0 text-black">
-					<h2 class="card-title text-2xl text-red-600">{team.name} {team.surname}</h2>
-					<p class=" text-lg">{team.role}</p>
-					<div class="flex flex-col">
-						<a href={`mailto:${team.email}`} class="link-hover text-sm">{team.email}</a>
+				<div class="card-body p-4 text-black">
+					<h2 class="card-title text-xl font-semibold text-red-600">{team.name} {team.surname}</h2>
+					<p class="text-lg text-gray-700">{team.role}</p>
+					<div class="mt-2 flex flex-col">
+						<a
+							href={`mailto:${team.email}`}
+							class="link-hover text-sm text-blue-500 hover:text-blue-700">{team.email}</a
+						>
 					</div>
 				</div>
 			</div>
