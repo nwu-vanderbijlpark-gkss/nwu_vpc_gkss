75293b33ac505768247f7b0e3b83f558c57458e1
lethabo <shysnylethabo11@gmail.com>
2025-03-08 17:47:37 +0200

    update: actions, keep alive to avoid supabase being paused

---
+++ netlify/functions/keep-alive.js
@@ -0,0 +1,19 @@
+export const handler = async (event, context) => {
+    try {
+        const url = 'https://nwu-vaal-gkss.netlify.app/community/api/getLeaderBoard';
+
+        const response = await fetch(url);
+  
+      if (!response.success) throw new Error("Keep-alive failed");
+      
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Supabase kept alive successfully" })
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: error.message })
+      };
+    }
+  };
