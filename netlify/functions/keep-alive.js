e76786c710827f55199c9e0e20dd39a464d1d946
lethabo <shysnylethabo11@gmail.com>
2025-03-10 13:29:34 +0200

    update: keep alive fix

--- netlify/functions/keep-alive.js
+++ netlify/functions/keep-alive.js
@@ -1,20 +1,23 @@
-export const handler = async (req) => {
-    const { next_run } = await req.json()
+export const handler = async ({locals:{ supabase }}) => {
     try {
-        const url = 'https://nwu-vaal-gkss.netlify.app/community/api/getLeaderBoard';
+      const url = 'https://nwu-vaal-gkss.netlify.app/community/api/getLeaderBoard';
 
-        const response = await fetch(url);
-  
-      if (!response.success) throw new Error("Keep-alive failed");
+        const response = await fetch(url, { method: "GET" });
+        const res = await response.json();
+        //also just call from the supabase api
+        const { data, error } = await supabase
+            .from('Comment')
+            .select('*');
+        if (error) throw new Error(error);
       
       return {
         statusCode: 200,
         body: JSON.stringify({ message: "Supabase kept alive successfully" })
       };
-    } catch (error) {
+    } catch (e) {
       return {
         statusCode: 500,
-        body: JSON.stringify({ error: error.message })
+        body: JSON.stringify({ error: e.message })
       };
     }
   };
