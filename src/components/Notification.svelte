d4feb992a1a6d348d5732a3c9d1fb42f14e362fe
lethabo <shysnylethabo11@gmail.com>
2025-03-31 01:04:20 +0200

    fix: display notification above everything, zIndex: 100, adding social links.

--- src/components/Notification.svelte
+++ src/components/Notification.svelte
@@ -17,7 +17,7 @@
 
 <div transition:fly={{ y: 20, duration: 300 }} class="toast">
 	<div
-		class="alert alert-{type} mb-2 flex items-center gap-3 rounded-lg p-4 text-sm font-medium shadow-lg"
+		class="notification alert alert-{type} mb-2 flex items-center gap-3 rounded-lg p-4 text-sm font-medium shadow-lg"
 		class:bg-success={type === 'success'}
 		class:bg-error={type === 'error'}
 	>
@@ -32,9 +32,6 @@
 
 <style>
 	.notification {
-		position: fixed;
-		bottom: 1rem;
-		right: 1rem;
 		z-index: 100;
 	}
 </style>
