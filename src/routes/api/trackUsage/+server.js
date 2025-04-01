8e5e106049f8d49038c5fbee2d5f7d9fca610876
lethabo <shysnylethabo11@gmail.com>
2025-04-01 20:29:58 +0200

    update: track every access to the website

--- src/routes/api/trackUsage/+server.js
+++ src/routes/api/trackUsage/+server.js
@@ -1,10 +1,8 @@
 import { json } from "@sveltejs/kit";
 
-export const POST = async({request, locals:{supabase}}) => {
-    const {data: {user}} = await supabase.auth.getUser();
-    const {location} = await request.json();
-    const {data,error} = await supabase.from("app_usage").insert({location: location,user_id: user.id});
-        
-        if(!error) return json({success: true});
-        else return json({success: false, error});
+export const POST = async ({ request, locals: { supabase } }) => {
+    const { location } = await request.json();
+    const { data, error } = await supabase.from("app_usage").insert({ location: location });
+    if (!error) return json({ success: true });
+    else return json({ success: false, error });
 }
