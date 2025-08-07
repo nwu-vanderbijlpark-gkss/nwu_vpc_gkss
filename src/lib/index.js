40bec42374d2ccd3cf60b232297ea17fbf39a606
lethabo <shysnylethabo11@gmail.com>
2025-08-07 20:07:31 +0200

    chore: remove the encryption key from /lib/index.js file and add new encryption key to env

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,7 @@
+import { CRYPTO_KEY } from '$env/static/private';
 import CryptoJS from 'crypto-js';
 // place files you want to import through the `$lib` alias in this folder.
-const key = "5e50f405ace6cbdf17379f4b9f2b0c9f4144c5e380ea0b9298cb02ebd8ffe511";
+const key = CRYPTO_KEY;
 
 export const encrypt = (text) => {
 	return CryptoJS.AES.encrypt(text,key).toString();
