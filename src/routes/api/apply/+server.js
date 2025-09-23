d81238dec0283885c2475f4d14367c1d8f56ba58
lethabo <shysnylethabo11@gmail.com>
2025-09-23 23:52:17 +0200

    update(apply): send email confirmation when the user applies for a role

--- src/routes/api/apply/+server.js
+++ src/routes/api/apply/+server.js
@@ -1,4 +1,6 @@
-export const POST = async ({locals: {supabase}, request}) => {
+import { gkssConfig } from '$lib/config';
+
+export const POST = async ({locals: {supabase}, request, fetch}) => {
     const {data: {user}} = await supabase.auth.getUser();
 
     if(!user) 
@@ -13,5 +15,32 @@ export const POST = async ({locals: {supabase}, request}) => {
     if(error) 
         return new Response(JSON.stringify({error: error.message}), {status: 500});
     
+
+    const emailMessage = `
+        <p style="font-family: Arial, sans-serif; font-size: 14px; color: #4a4a4a; margin: 10px 0;">
+            We have received your application for the position of <b style="color: #005b99;">${role}</b> with the ${gkssConfig.name}.<br><br>
+            Best regards,<br>
+            ${gkssConfig.name} Recruitment Team
+        </p>
+        `;
+    const subject = 'Application confirmation';
+
+    const { data:  member  } = await supabase.from("member").select("*").eq("id", user.id).single();
+    const req = await fetch("/api/sendEmail", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    data: {
+                        fullName: member.name + " " + member.surname,
+                        email: member.email,
+                        subject: subject,
+                        message: emailMessage,
+                        type: 'broadcast'
+                    }
+                })
+            })
+    const res = await req.json();
     return new Response(JSON.stringify({success: "Application submitted successfully"}), {status: 200});
 }
