3fbeb47284755be4403801c5d7f7dfd915ababbd
lethabo <shysnylethabo11@gmail.com>
2025-10-08 21:09:09 +0200

    fix, team page, removed an unused for loop

--- src/routes/(static pages)/team/+page.server.js
+++ src/routes/(static pages)/team/+page.server.js
@@ -1,17 +1,13 @@
 export const load = async({locals:{supabase}}) => {
 
+    let members = [];
     //load the team
     const {data, error} = await supabase.from("team").select("*, member(*)").order("role", {ascending: true});
     if(error){
         console.error(error);
         return {error: "Unexpected error occurred"};
     }
-
-    for(let member of Member){
-        let publicUrl = member.image ? await supabase.storage.from("files").getPublicUrl(member?.image?.substring(member.image?.indexOf("/"))): null;//removing the first "file/"
-        member = {...member,image: publicUrl?.data?.publicUrl ? publicUrl.data.publicUrl : "/temp/avatar.jpeg"};
-        members.push(member);
-    }
+    
     for(let i = 0; i< data.length; i++){
         let publicUrl = data[i].member.image ? await supabase.storage.from("files").getPublicUrl(data[i].member.image.substring(data[i].member.image.indexOf("/")+1)) : null;//removing the first "file/"
         data[i].member.image = publicUrl ? publicUrl.data.publicUrl : "/temp/avatar.jpeg";
